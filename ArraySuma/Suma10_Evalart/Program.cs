4c4a8356111ff076498bff3e7c9f8f24b9920e49
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-08-09 11:00:58 -0500

    Add project

---
+++ ArraySuma/Suma10_Evalart/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    static int[] myArray = new int[] { 1, 3, 4, 2, 7, 0 };
+
+    public static void Main()
+    {
+        FindElementsWithSum10(myArray);
+    }
+
+    static void FindElementsWithSum10(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] + array[j] == 10)
+                {
+                    Console.WriteLine(array[i] + " " + array[j]);
+                    return;
+                }
+            }
+        }
+    }
+
+}
